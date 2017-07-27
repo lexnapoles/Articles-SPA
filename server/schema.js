164e49384c42059414be5e65de1a22e39cde817e
Alejandro Napoles <alejandro@alejandronapoles.com>
2017-07-27 23:36:22 +0200

    Created deleteArticle mutation.

--- server/schema.js
+++ server/schema.js
@@ -4,6 +4,7 @@ import {
   GraphQLNonNull,
   GraphQLString,
   GraphQLList,
+  GraphQLInputObjectType,
   GraphQLSchema,
 } from 'graphql';
 import db from './db';
@@ -61,8 +62,38 @@ const Query = new GraphQLObjectType({
   }),
 });
 
+const deleteArticleInputType = new GraphQLInputObjectType({
+  name: 'DeleteArticleInput',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+      description: 'The id of the article',
+    },
+  }),
+});
+
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'This is a root mutation',
+  fields: () => ({
+    deleteArticle: {
+      type: GraphQLString,
+      args: {
+        article: {
+          type: new GraphQLNonNull(deleteArticleInputType),
+        },
+      },
+      resolve(_, { article }) {
+        return db.Article.findByIdAndRemove(article.id)
+          .then(({ id }) => id);
+      },
+    },
+  }),
+});
+
 const Schema = new GraphQLSchema({
   query: Query,
+  mutation: Mutation,
 });
 
 export default Schema;
