830d846093209b64b62a0c066519c946e6f80052
Alejandro Napoles <alejandro@alejandronapoles.com>
2017-07-27 12:51:20 +0200

    Installed React-Router 4. Added two routes in App, one for the list and another for the article.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './app.css';
 import ConnectedArticlesList from '../articlesList/ConnectedArticlesList';
+import Article from '../article/Article';
 
 const App = () =>
   <div className="app" >
     <header className="header" />
     <main className="content" >
-      <ConnectedArticlesList />
+      <BrowserRouter >
+        <Switch >
+          <Route exact path="/" component={ConnectedArticlesList} />
+          <Route path="/:id" component={Article} />
+        </Switch >
+      </BrowserRouter >
     </main >
     <footer className="footer" />
   </div >;
--- src/components/articlesList/ArticlesList.js
+++ src/components/articlesList/ArticlesList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { articlesPropType } from '../propTypes';
 import ArticleOverview from './articleOverview/ArticleOverview';
-import '../../b-list.css';
+import './b-list.css';
 
 const ArticlesList = ({ articles }) => (
   <div className="b-list" >
