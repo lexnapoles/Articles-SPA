ccd754d9c7ca7e4561320a8e9daa7eaa6f1a441e
Alejandro Napoles <alejandro@alejandronapoles.com>
2017-07-28 02:35:38 +0200

    Replaced delete button with icon.

--- src/components/articlesList/articleOverview/ArticleOverview.js
+++ src/components/articlesList/articleOverview/ArticleOverview.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import MdDeleteIcon from 'react-icons/lib/md/delete';
 import { articleOverviewPropType } from '../../propTypes';
+import './b-toolbar.css';
 
 const ArticleOverview = ({ article, onClick, onDelete }) => {
   const { author, excerpt, title } = article;
@@ -11,7 +13,9 @@ const ArticleOverview = ({ article, onClick, onDelete }) => {
 
   return (
     <section className="b-list__item" onClick={onClick} >
-      <button onClick={onDeleteHandler} >Delete</button >
+      <div className="b-toolbar" >
+        <MdDeleteIcon className="b-toolbar__item" size={20} onClick={onDeleteHandler} />
+      </div >
       <h1 >{title}</h1 >
       <h2 >{author}</h2 >
       <p >{excerpt}</p >
