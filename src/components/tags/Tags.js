795cc19da859c22ad3f8526aa4b0022c80daed01
Alejandro Napoles <alejandro@alejandronapoles.com>
2017-08-10 19:47:17 +0200

    Limited tag lenght.

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -4,7 +4,7 @@ import './b-tags.css';
 
 const Tags = ({ tags }) =>
   <div className="b-tags__container" >
-    {tags.map(tag => <span key={tag} className="b-tags__item" >{tag}</span >)}
+    {tags.map(tag => <div key={tag} className="b-tags__item" >{tag}</div >)}
   </div >;
 
 Tags.propTypes = {
